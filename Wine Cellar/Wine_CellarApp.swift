83aecbdc029879b100e39609d4901589c14642db
Liam Schmid <mail@liamschmid.com>
2025-10-29 16:22:54 +0100

    Add files via upload

---
+++ Wine Cellar/Wine_CellarApp.swift
@@ -0,0 +1,32 @@
+//
+//  Wine_CellarApp.swift
+//  Wine Cellar
+//
+//  Created by Liam Schmid on 29.10.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Wine_CellarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Wine CellarTests/Wine_CellarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wine_CellarTests.swift
+//  Wine CellarTests
+//
+//  Created by Liam Schmid on 29.10.25.
+//
+
+import Testing
+@testable import Wine_Cellar
+
+struct Wine_CellarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
